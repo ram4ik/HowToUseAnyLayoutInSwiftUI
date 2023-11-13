47c58d92429327199a2fe46678cf3fd4ee438143
ram4ik <ram4ik@hotmail.com>
2023-11-13 20:53:53 +0200

    How to use AnyLayout in SwiftUI

--- HowToUseAnyLayoutInSwiftUI/ContentView.swift
+++ HowToUseAnyLayoutInSwiftUI/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @Environment(\.verticalSizeClass) private var verticalSizeClass
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 12) {
+            Text("Horizontal: \(horizontalSizeClass.debugDescription)")
+            Text("Vertical: \(verticalSizeClass.debugDescription)")
+            
+            let layout: AnyLayout = horizontalSizeClass == .compact ? AnyLayout(VStackLayout()) : AnyLayout(HStackLayout())
+            
+            layout {
+                Text("Alpha")
+                Text("Beta")
+                Text("Gamma")
+            }
+            
+//            if horizontalSizeClass == .compact {
+//                VStack {
+//                    Text("Alpha")
+//                    Text("Beta")
+//                    Text("Gamma")
+//                }
+//            } else {
+//                HStack {
+//                    Text("Alpha")
+//                    Text("Beta")
+//                    Text("Gamma")
+//                }
+//            }
         }
-        .padding()
     }
 }
 
