897f4a6f93822df463375bcf632bf0fa247ad6f1
ram4ik <ram4ik@hotmail.com>
2023-11-13 20:25:22 +0200

    Initial Commit

---
+++ HowToUseAnyLayoutInSwiftUI/HowToUseAnyLayoutInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseAnyLayoutInSwiftUIApp.swift
+//  HowToUseAnyLayoutInSwiftUI
+//
+//  Created by admin on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseAnyLayoutInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
